8cd53e3c6a1297c7c62e277e68a888bd54a62efb
matias lindgren <matias.lindgren@aalto.fi>
2016-03-25 10:52:03 +0200

    added functionalities

--- a_module.py
+++ a_module.py
@@ -1,7 +1,7 @@
 def remove_vowels(a_string):
-    return a_string
-
     vowels = set(["a", "e", "i",
                 "o", "u", "y" ])
     return ''.join(c for c in a_string if str.lower(c) not in vowels)
-    
+
+def dummy_fun():
+    raise(NotImplementedError)
